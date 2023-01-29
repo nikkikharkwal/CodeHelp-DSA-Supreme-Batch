2b10252186fff58ba407739288fe7b506b4c6d69
nikkikharkwal <nikki.kharkwal@gmail.com>
2023-01-29 12:32:09 +0530

    if-else

---
+++ Lecture - 3/if-else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int bronum;
+  cin >> bronum;
+
+  if(bronum == 0){
+    cout << "Baat Ban Jayegi" << endl;
+  }
+  else {
+    cout << "Baat Nhi Banegi" << endl;
+  }
+}
