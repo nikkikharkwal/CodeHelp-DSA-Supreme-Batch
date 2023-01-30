6d25b5642cbc9ca42a31866510948b31ec87f0e0
nikkikharkwal <nikki.kharkwal@gmail.com>
2023-01-30 09:03:34 +0530

    Solid Parallelogram

---
+++ Patterns/SolidParallelogram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N; //No. Of Rows
+  cin >> N;
+
+  for(int i = 0;i < N;i++){
+    for(int j = 0; j < N - i; j++){
+      cout << "  ";
+    }
+    for(int k = 0; k < i; k++){
+      cout << " * ";
+    }
+    for(int z = i;z < N - 1;z++){
+      cout << " * ";
+    }
+    cout << endl;
+  }
+}
