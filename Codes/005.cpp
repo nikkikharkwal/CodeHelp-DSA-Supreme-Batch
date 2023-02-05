e1f18b38ff45bf390e63e75e17e6eb6fc5dc9034
nikkikharkwal <nikki.kharkwal@gmail.com>
2023-02-05 15:15:18 +0530

    Lecture 5

--- Codes/005.cpp
+++ Codes/005.cpp
@@ -1,161 +1,171 @@
 #include <iostream>
 using namespace std;
 
-void printName() {
-	int n;
-	cout<<"Enter the value of n " << endl;
-
-	cin >> n;
-	for(int i=0; i<n; i++) {
-		cout << "babbar" << endl;
-	}
-}
-
-void printNumber(int num) {
-	cout << num << endl;
-}
-
-
-int add(int x, int y  );
-
-int findMax(int num1, int num2, int num3) {
-	if(num1 > num2 && num1 > num3) {
-		return num1;
-	}
-	else if(num2 > num1 && num2 > num3) {
-		return num2;
-	}
-	else {
-		return num3;
-	}
-}
-
-//n is a parameter
-void printCounting(int n) {
-	for(int i=1; i<=n; i++) {
-		cout << i << " ";
-	}
-	cout << endl;
-}
-
-char getGrade(int marks) {
-	// if(marks >= 90) 
-	// 	return 'A';
-	// else if(marks >= 80)
-	// 	return 'B';
-	// else if(marks >= 70)
-	// 	return 'C';
-	// else if(marks >= 60) 
-	// 	return 'D';
-	// else
-	// 	return 'E';
-
-	//assuming marks is between 0 to 100
-	switch(marks/10){
-		case 10 : return 'A'; break;
-		case 9  : return 'A'; break;
-		case 8  : return 'B'; break;
-		case 7  : return 'C'; break;
-		case 6  : return 'D'; break;
-		default: return 'E';
-	}
-}
-
-int getSum(int n) {
-	int sum = 0;
-	for(int i=1; i<=n; i++) {
-		sum = sum + i;
-	}
-
-	return sum;
-}
-
-int getEvenSum(int n) {
-	cout << "inside getEveSum Fiunction" << endl;
-	cout << "Vlaue of n is " << n << endl;
-	
-	int sum = 0 ;
-	cout << "Initial value of sum is " << sum << endl;
-	for(int i = 2; i<=n; i = i + 2) {
-		cout << "for value of i: " << i << endl;
-		sum = sum + i;
-		cout << "sum now become: " << sum << endl;
-	}
-	cout << "returning sum = " << sum << endl;
-	return sum;
-}
+int raja = 420;
 
 int main() {
 
-	//function call
-	//printName();
 
-	// int a = 5;
-	// cout << "Address of a is: " << &a << endl;
+
+	int val;
+	cout << "Enter the value " << endl;
+
+	cin >> val;
+
+	switch(val) {
+		case 1: cout << "Love ";
+		break;
+		case 2: cout << "Babbar";
+		break;
+		case 3: cout << "Ramesh";
+		break;
+		case 4: cout << "Rahul";
+		break;
+		default: cout << "Suresh";
+	}
+
 	
-	// printNumber(a);
+	//vairable scoping
 
-	// int a;
-	// cout << "Enter the value of a " << endl;
-	// cin >> a;
+	//decalaration
+	int a;
+	//initialisation
+	int b = 5;
+	//updation
+	// b = 10;
+	// cout << raja << endl;
 
-	// int b;
-	// cout << "Enter the value of b " << endl;
-	// cin >> b;
+	// if(true) {
+	// 	int b = 15;
+	// 	int raja = 421;
 
-	// int sum = add(a, b);
-
-	// cout << "Addition result is: " << sum << endl;
-
-
-	// int a,b,c;
-	// cin >> a >> b >> c;
-
-	// int maximumNumber = findMax(a,b,c);
-	// cout << maximumNumber << endl;
-
-	// int n;
-	// cout << "Enter the value of n" << endl;
-	// cin >> n;
-
-	// //n is argument
-	// printCounting(n);
-
-
-	// int marks;
-	// cout << "Enter the marks: " << endl;
-	// cin >> marks;
-
-	// char finalGrade = getGrade(marks);
-	// cout << finalGrade << endl;
-
-	// for(int i=0; i<=100; i++) {
-	// 	char ans = getGrade(i);
-	// 	cout << "Grade for marks = " << i << " is " << ans << endl;
+	// 	cout << b << endl;
+	// 	cout << raja << endl;
 	// }
 
-	// int n;
-	// cout << "Enter the value of n " << endl;
-	// cin >> n;
+	// cout << b << endl;
 
-	// int ans = getSum(n);
 
-	// cout << "Sum upto " << n << " is  " << ans << endl;
 
-	int n;
-	cout << "Enter the value of n " << endl;
-	cin >> n;
 
-	cout << "Calling getEvenSum function " << endl;
-	int ans = getEvenSum(n);
-	cout << "Ebven sum is  " << ans << endl;
 	
-	return 0;
-}
+	// for(int i=0; i<5; i++) {
+	// 	continue;
+	// 	cout << "Babbar" << endl ;
+	// }
+	
+	// int n = 5;
+	// for(int i=0; i<n; i++) {
+	// 	cout << "Babbar" << endl;
+	// 	break;
+	// }
+	// cout << "Love " << endl;	
+	// int a = 5;
+	// cout << (++a) * (++a) << endl;
+
+	// int a = 6;
+	// int c = a++ + 1;
+
+	// cout << c;
+
+	// int a = 11;
+	// cout << a++;
+
+	// cout<< endl << a << endl;
+	
+	// int a = -27;
+	// a = a >> 1;
+	// cout << a << endl;
+	
+	// int a  = 12;
+	// a = a << 3;
+	// cout << a << endl;
+	
+	// bool a = false;
+	// bool b = true;
+
+	// a = (~a);
+	// a = ~(a);
+	// cout << (~a) << endl;
+	// //cout <<  (a | b) ;
+ // 	// cout << (a^b) ;
+
+	// cout << (5 & 10 ) << endl;
+	
+ //  int n;
+	// cin >>n;
 
 
-int add(int x, int y  )
-{
-	int result = x + y;
-	return result;
+	// for(int row=0; row<n; row = row + 1) {
+	// 	//spaces
+	// 	for(int col=0; col < n-row-1; col = col +1) {
+	// 		cout << " ";
+	// 	}
+
+	// 	//numerbers with spaces in between
+	// 	int start = 1;
+	// 	for(int col =0; col < 2*row+1; col = col + 1) {
+	// 		//first row or last row 
+	// 		if(row == 0 || row == n-1) {
+	// 			if(col %2 == 0) {
+	// 				//even
+	// 				cout << start;
+	// 				start = start + 1;
+	// 			}
+	// 			else {
+	// 				//odd
+	// 				cout << " ";
+	// 			}
+	// 		}
+	// 		else {
+	// 			//first col
+	// 			if(col == 0) {
+	// 				cout << 1;
+	// 			}
+	// 			else if(col == 2*row+1-1){
+	// 				cout << row+1;
+	// 			}
+	// 			else {
+	// 				cout <<" ";
+	// 			}
+	// 		}
+	// 	}
+	// 	cout << endl;
+	// }
+	
+	// for(int row=0; row < n; row = row + 1) {
+	// 	//spaces
+	// 	for(int col =0; col <n-row-1; col = col + 1) {
+	// 		cout << " ";
+	// 	}
+
+	// 	//numbers
+	// 	for(int col = 0; col < row+1; col = col + 1) {
+	// 		cout << row + col + 1 ;
+	// 	}
+		
+
+	// 	//reverse counting
+	// 	int start = 2*row;
+	// 	for(int col=0; col <row; col = col + 1) {
+	// 		cout << start;
+	// 		start = start - 1;
+	// 	}
+
+
+	// 	cout << endl;
+	// }
+
+	// for(int row=0; row<n; row = row + 1) {
+
+	// 	//row+1 elements print krne wale hai
+	// 	int start = row+1;
+	// 	for(int col = 0; col < row+1; col = col + 1) {
+	// 	//	cout << row + col + 1 << " ";
+	// 		cout << start <<" ";
+	// 		start = start + 1;
+	// 	}
+	// 	cout << endl;
+		
+	// }
 }
